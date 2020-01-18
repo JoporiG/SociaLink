29d3aec6fc4b1aefcbadf827005fd8f7c32f0811
ABC123-crypto <mishkavyun2@gmail.com>
2020-01-18 15:02:35 +0300

    Profile added

--- link/src/App.vue
+++ link/src/App.vue
@@ -1,13 +1,66 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
-</template>
+  <v-app id="app">
+    <v-navigation-drawer color="green" dark 
+                                      expand-on-hover 
+                                      hide-overlay 
+                                      permanent
+                                      right
+                                      app>
+      <v-list nav 
+              shaped
+              dense>
+        <v-list-item two-line>
+          <v-list-item-avatar>
+            <img src="https://randomuser.me/api/portraits/men/1.jpg">
+          </v-list-item-avatar>
 
+          <v-list-item-content class="text-left">
+            <v-list-item-title class="font-weight-black">SocialLink</v-list-item-title>
+            <v-list-item-subtitle>Иван Иванов</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+        <v-divider class="my-3"/>
+        <v-list-item link 
+                          :to="link.path"
+                          :key="link.path"
+                          v-for="link in links">
+          <v-list-item-icon>
+            <v-icon>{{link.icon}}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            {{link.label}}
+          </v-list-item-content>
+        </v-list-item>
+
+      </v-list>
+    </v-navigation-drawer>
+    <v-content class="px-12 py-3">
+      <v-container fluid>
+        <router-view/>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+<script>
+export default {
+  data(){
+    return {
+      links: [
+        {
+          label: "Главная",
+          path: '/', 
+          icon: "mdi-home-outline",
+        },
+        {
+          label: "Профиль",
+          path: '/profile', 
+          icon: "mdi-account"
+        },
+      ]
+    }
+  }
+}
+</script>
 <style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
